b388931815ebd15de5f414afe08c1fa15ddd2426
Luiz Paulo Caetano <luizcaetano0289@gmail.com>
2022-04-11 18:09:40 -0300

    ListView - C#

--- App00_ControleXF/App00_ControleXF/obj/Debug/netstandard2.0/Controles/ListViewPage.xaml.g.cs
+++ App00_ControleXF/App00_ControleXF/obj/Debug/netstandard2.0/Controles/ListViewPage.xaml.g.cs
@@ -17,8 +17,12 @@ namespace App00_ControleXF.Controles {
     public partial class ListViewPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.ListView ListaPessoas;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(ListViewPage));
+            ListaPessoas = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.ListView>(this, "ListaPessoas");
         }
     }
 }
